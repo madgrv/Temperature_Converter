559f51a04aa6fbb2f133d20db339e8bc62a4d775
madgrv <32220943+madgrv@users.noreply.github.com>
2023-02-27 03:13:54 +0000

    Add files via upload

--- Temperature_Converter/T06 temperature.js
+++ Temperature_Converter/T06 temperature.js
@@ -1,6 +1,6 @@
 // Write the code that will do the following:
 /* 1. The user should be allowed to choose which calculation they want to do. The first output that the 
-user sees when the program runs should say:
+      user sees when the program runs should say:
 //             In which metric is the temperature you are converting?
 //             C - Celsius
 //             F - Fahrenheit
@@ -12,26 +12,24 @@ user sees when the program runs should say:
 //             F - Fahrenheit
 //             K - Kelvin
 // 4. The program should print out the answer in the following format:
-// -273°C is 0K. (there's a typo in the text here it should be 0.15K)
+//    -273°C is 0K. (there's a typo in the text here it should be 0.15K)
 // 5. The program should be able to convert from any of the three metrics to any of the three metrics.
-// Conversion formulae:
+//    Conversion formulae:
+      //● Celsius from:
+          ○ Fahrenheit: C = (F -32) x 5/9
+          ○ Kelvin: C = K - 273.15
 
-//● Celsius from:
-○ Fahrenheit: C = (F -32) x 5/9
-○ Kelvin: C = K - 273.15
+      ● Fahrenheit from:
+          ○ Celsius:F=Cx9/5+32
+          ○ Kelvin:F=kx9/5-459.67
 
-● Fahrenheit from:
-○ Celsius:F=Cx9/5+32
-○ Kelvin:F=kx9/5-459.67
-
-● Kelvin from:
-○ Celsius: K = C + 273.15
-○ Fahrenheit: K = (F + 459.67) x 5/9 */
-
+      ● Kelvin from:
+          ○ Celsius: K = C + 273.15
+          ○ Fahrenheit: K = (F + 459.67) x 5/9 */
 
 
 
-// Error handling was not requested (nor taught yet)at the time of the assignment but I have added it doing my own research to make a complete program//
+// Error handling was not requested (nor taught yet) at the time of the assignment but I have added it doing my own research to make a complete program//
 
 
 
